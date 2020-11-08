cc6dd52dcb78dde846c3b286127e64483ba89ae7
Mizuguchi <makoto.mizuguchi@rakuten.com>
2020-11-09 00:29:16 +0900

    third

--- youtube.py
+++ youtube.py
@@ -76,7 +76,7 @@ def main():
             df_static = pd.DataFrame(list(df['videoId'].apply(lambda x : get_statistics(x))))
             df_output = pd.concat([df,df_static], axis = 1)
             df_output.to_csv("./youtube.csv")
-            yt = YTstats(API_KEY, channel_id)
+            yt = YTstats(YOUTUBE_API_KEY, channel_id)
             yt.extract_all()
             yt.dump()  # dumps to .json
             st.dataframe(df_output)
