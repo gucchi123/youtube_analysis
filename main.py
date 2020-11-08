c78416043606b6d6763aaae3cd3b250a4ea7332d
Mizuguchi <makoto.mizuguchi@rakuten.com>
2020-11-08 23:39:44 +0900

    third

---
+++ main.py
@@ -0,0 +1,15 @@
+from youtube_statistics import YTstats
+
+API_KEY = 'AIzaSyBD2iQAO6EFpiVdcKmt0-L5xEi6GyV75K8'
+channel_id = "UCYp373JfGNTCu_M9AGhg7tA"
+
+#python_engineer_id = 'UCbXgNpp0jedKWcQiULLbDTA'
+#channel_id = python_engineer_id
+
+#yt = YTstats(API_KEY, channel_id)
+#yt.get_channel_statistics()
+#yt.get_channel_video_data()
+#yt.dump()
+yt = YTstats(API_KEY, channel_id)
+yt.extract_all()
+yt.dump()  # dumps to .json
